29a16c60cdce4d94c5698b0a14df34dd893b67ac
Jordan Chapman <jchapman@ara.com>
2016-08-18 11:53:39 -0400

    Updated css/style

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,7 +12,7 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = 'en'
 
-THEME = '../themes/hmleal-theme'
+THEME = '../themes/ninja-theme'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
